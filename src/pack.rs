d916432652ca780dc4c954eeae642e85acf98684
Sebastian Reinhard <sebastian@sebbert.me>
2017-03-03 22:59:46 +0100

    Inline pack functions

--- src/pack.rs
+++ src/pack.rs
@@ -1,3 +1,4 @@
+#[inline]
 pub fn pack_u16(hi: u8, lo: u8) -> u16 {
 	let hi = hi as u16;
 	let lo = lo as u16;
@@ -5,6 +6,7 @@ pub fn pack_u16(hi: u8, lo: u8) -> u16 {
 	(hi << 8) | lo
 }
 
+#[inline]
 pub fn unpack_u16(value: u16) -> (u8, u8) {
 	let hi = (value >> 8) as u8;
 	let lo = value as u8;
@@ -12,10 +14,12 @@ pub fn unpack_u16(value: u16) -> (u8, u8) {
 	(hi, lo)
 }
 
+#[inline]
 pub fn pack_nibbles(hi: u8, lo: u8) -> u8 {
 	(hi << 4) | (lo & 0x0f)
 }
 
+#[inline]
 pub fn unpack_nibbles(value: u8) -> (u8, u8) {
 	let hi = value >> 4;
 	let lo = value & 0x0f;
