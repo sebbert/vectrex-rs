3ca3ffbc3a47384dc8438cb0b78148f7ac0a7a8c
Sebastian Reinhard <sebastian@sebbert.me>
2017-03-04 12:08:05 +0100

    Begin implementing VIA timer 1

--- src/via.rs
+++ src/via.rs
@@ -1,32 +1,73 @@
+#![allow(dead_code)]
+
+use pack::*;
+
+const ADDR_T1_COUNTER_LO: u16 = 4;
+const ADDR_T1_COUNTER_HI: u16 = 5;
+const ADDR_T1_LATCH_LO: u16 = 6;
+const ADDR_T1_LATCH_HI: u16 = 7;
+
+#[derive(Default)]
 pub struct Via {
-	
+	t1_interrupt_enabled: bool,
+
+	t1_counter_lo: u8,
+	t1_counter_hi: u8,
+	t1_latch_lo: u8,
+	t1_latch_hi: u8,
 }
 
 impl Via {
 	pub fn new() -> Via {
-		Via {}
+		Default::default()
 	}
 
 	pub fn read(&self, addr: u16) -> u8 {
 		let addr = Self::mask_addr(addr);
 		match addr {
+			4 => {
+				0
+			}
+			5 => self.t1_counter_hi,
 			_ => {
-				warn!("VIA {:01x}", addr);
-				0 
+				error!("Read from unimplemented VIA reg {:01x}", addr);
+				0
 			}
 		}
 	}
 
-	pub fn write(&self, addr: u16, value: u8) {
+	pub fn write(&mut self, addr: u16, value: u8) {
 		let addr = Self::mask_addr(addr);
 		match addr {
-			_ => {
-				warn!("VIA {:01x} = {:02x}", addr, value);
-			}
+			ADDR_T1_COUNTER_LO | ADDR_T1_LATCH_LO => {
+				self.t1_latch_lo = value;
+			},
+
+			_ => warn!("Write to unimplemented VIA reg {:01x} = {:02x}", addr, value)
 		}
 	}
 
 	fn mask_addr(addr: u16) -> u16 {
-		addr & 0x000f
+		addr & 0xf
+	}
+
+	fn t1_counter(&self) -> u16 {
+		pack_u16(self.t1_counter_hi, self.t1_counter_lo)
+	}
+
+	fn t1_latch(&self) -> u16 {
+		pack_u16(self.t1_latch_hi, self.t1_latch_lo)
+	}
+
+	fn set_t1_counter(&mut self, value: u16) {
+		let (hi, lo) = unpack_u16(value);
+		self.t1_counter_hi = hi;
+		self.t1_counter_lo = lo;
+	}
+
+	fn set_t1_latch(&mut self, value: u16) {
+		let (hi, lo) = unpack_u16(value);
+		self.t1_latch_hi = hi;
+		self.t1_latch_lo = lo;
 	}
 }
