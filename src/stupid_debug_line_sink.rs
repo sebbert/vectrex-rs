fda772c9af72456fc72d5b9c821c11bfa2d5d5cc
Sebastian Reinhard <sebastian@sebbert.me>
2017-05-14 01:44:31 +0200

    Fix sink

--- src/stupid_debug_line_sink.rs
+++ src/stupid_debug_line_sink.rs
@@ -1,11 +1,12 @@
 use line_sink::{Line, LineSink};
+use vec2::*;
 
 pub struct StupidDebugLineSink {}
 
 impl LineSink for StupidDebugLineSink {
 	fn append(&mut self, line: Line) {
-		let (x0, y0) = line.start;
-		let (x1, y1) = line.end;
-		println!("Draw line from ({}, {}) to ({}, {})", x0, y0, x1, y1);
+		let Vec2 { x: x0, y: y0 } = line.start;
+		let Vec2 { x: x1, y: y1 } = line.end;
+		debug!("Draw line from ({}, {}) to ({}, {})", x0, y0, x1, y1);
 	}
 }
